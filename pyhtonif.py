e9323815c767ba38c15a3a1e91ff568d68bd83a5
CGWhiteCompSci <155092416+CGWhiteCompSci@users.noreply.github.com>
2024-12-06 08:33:07 +0000

    Add files via upload

---
+++ pyhtonif.py
@@ -0,0 +1,12 @@
+finalgrade = 20
+
+if finalgrade >= 70:
+    print ("thats a first")
+elif finalgrade >= 60:
+    print("thats a 2:1")
+elif finalgrade >= 50:
+    print("thats a 2:2")
+elif finalgrade >= 40:
+    print("thats a third")
+else:
+    print("you have failed")
